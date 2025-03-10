932d6f0d38f5c4ede1e24c89076cd29c48404bd6
João Saraiva <joao.saraiva@diconium.com>
2025-03-10 21:56:06 +0000

    add logic to author the headings of the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,67 @@
+"use client";
 import {BlogGridComponent} from "@/components/blog-grid/blog-grid-component";
-import {Suspense} from "react";
+import {Suspense, useEffect, useState} from "react";
+import {getHeadingRes} from "@/services/heading/heading-service";
+import {usePathname} from "next/navigation";
 
-export default async function Home() {
+
+export default function Home() {
+
+    const entryUrl = usePathname()
+    const [mainHeadings, setMainHeadings] = useState({
+        mainHeading: '',
+        subHeading: ''
+    });
+
+    const [newsletterHeadings, setNewsletterHeadings] = useState({
+        mainHeading: '',
+        subHeading: ''
+    });
+
+    const mapHeadingsResponse = (response: any) => {
+
+        setMainHeadings((prev) => ({
+            ...prev,
+            mainHeading: response?.headings?.children[0]?.children[0]?.text,
+            subHeading: response?.headings?.children[1]?.children[0]?.text,
+        }))
+    }
+
+    const mapNewsletterHeadingsResponse = (response: any) => {
+
+        setNewsletterHeadings((prev) => ({
+            ...prev,
+            mainHeading: response?.headings?.children[0]?.children[0]?.text,
+            subHeading: response?.headings?.children[1]?.children[0]?.text,
+        }))
+    }
+
+    async function fetchData() {
+        try {
+            const mainHeadingsRes = await getHeadingRes({entryUrl: '/'});
+            const newsletterHeadingsRes = await getHeadingRes({entryUrl: '/newsletter'});
+
+            if (!mainHeadingsRes || !newsletterHeadingsRes) throw new Error('Status code 404');
+
+            mapHeadingsResponse(mainHeadingsRes)
+            mapNewsletterHeadingsResponse(newsletterHeadingsRes)
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
 
   return (
         <>
             <section className="mb-16 grid gap-y-14">
                 <div className=" text-center grid gap-y-5">
-                    <h1 className="text-4xl font-bold mb-4 text-[#3E2723]">Recipes blog</h1>
+                    <h1 className="text-4xl font-bold mb-4 text-[#3E2723]">{mainHeadings?.mainHeading}</h1>
                     <p className="max-w-2xl mx-auto text-[#5A5A5A]">
-                        Discover simple, tasty recipes that anyone can make. From quick weeknight dinners to impressive dinner
-                        party dishes.
+                        {mainHeadings?.subHeading}
                     </p>
                 </div>
                 <Suspense fallback={<div>Loading recipes...</div>} >
@@ -20,9 +71,9 @@ export default async function Home() {
 
             <section className="border-t py-16 ">
                 <div className="text-center max-w-2xl mx-auto">
-                    <h2 className="text-3xl font-bold mb-4">Subscribe to our newsletter</h2>
+                    <h2 className="text-3xl font-bold mb-4">{newsletterHeadings?.mainHeading}</h2>
                     <p className="mb-6">
-                        Get the latest recipes sent straight to your inbox.
+                        {newsletterHeadings?.subHeading}
                     </p>
                     <div className="flex flex-col sm:flex-row gap-2 max-w-md mx-auto">
                         <input
